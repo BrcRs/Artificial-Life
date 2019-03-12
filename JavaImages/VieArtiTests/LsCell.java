1c3722047ac0c6086c338a1dda0877dae8290a84
BrcRs <47972925+BrcRs@users.noreply.github.com>
2019-03-12 10:18:56 +0100

    CellularAutomata pour Landscape
    Dossier test pour un automate cellulaire qui construit une image en teintes de gris. L'automate crée un bruit de Perlin ainsi qu'un embryon de volcan. Des ajustements sont sûrement à faire, et les fichiers doivent être réintégrés au sein du projet.

---
+++ JavaImages/VieArtiTests/LsCell.java
@@ -0,0 +1,33 @@
+
+public class LsCell {
+	public int whiteness;
+
+	public enum CellType
+	{
+	    NORMAL, VOLCANO_UP, VOLCANO_DOWN, STOP, NORMALSTOP;
+	}
+
+	public CellType mode;
+
+	public int parameter;
+
+	public LsCell(int _whiteness, CellType _mode, int _parameter)
+	{
+		this.whiteness = _whiteness;
+		this.mode = _mode;
+		this.parameter = _parameter;
+
+	}
+
+	public LsCell()
+	{
+		this((int)(255/2.), CellType.NORMAL, 0);
+	}
+
+	public LsCell clone()
+	{
+		return new LsCell(this.whiteness, this.mode, this.parameter);
+	}
+
+
+}
